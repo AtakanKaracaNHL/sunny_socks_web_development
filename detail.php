2c75bd3b7e41b14d521bba8e840c95f662c12f8c
Friso-klinge <friso.klinge@student.nhlstenden.com>
2025-10-09 13:35:02 +0200

    adding stylesheet and logo links

--- detail.php
+++ detail.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>detail</title>
+    <link rel="shortcut icon" href="img/illustraties/png/Sunny_socks_green.png"/>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Startpagina</title>
+    <link rel="shortcut icon" href="img/illustraties/png/Sunny_socks_green.png"/>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
