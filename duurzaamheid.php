c35aacf700da79a8b4972e2c702bc8455cf0f445
Giovanni Berlijn <giovanniberlijn@gmail.com>
2025-10-13 09:58:27 +0200

    Made the banner for the duurzaaheids page.

--- duurzaamheid.php
+++ duurzaamheid.php
@@ -5,9 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Duurzaamheid</title>
     <link rel="shortcut icon" href="img/illustraties/png/Sunny_socks_green.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-duurzaamheid.css">
 </head>
 <body>
+    <div class="title">
+        <h1>
+            <i class="fa-solid fa-recycle recycle-white"></i>
+            Duurzame Sokkenproductie
+            <i class="fa-solid fa-recycle recycle-white"></i>
+        </h1>
+            <p>Ontdek hoe sokken worden gemaakt van 100% gerecyclede materialen. Een <br>
+            eenvoudige roadmap die het complete proces toont.</p>  
+            <p> <i class="fa-solid fa-arrow-down arrow-green"></i> Scroll om het proces te bekijken <i class="fa-solid fa-arrow-down arrow-green"></i> </p>
+    </div>
     
 </body>
 </html>
