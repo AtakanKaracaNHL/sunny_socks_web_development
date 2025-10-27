d115b5f37f360e0d284fc8ae7f20395dce32c9ae
Giovanni Berlijn <giovanniberlijn@gmail.com>
2025-10-27 09:48:33 +0100

    Made the process line next to the steps

--- duurzaamheid.php
+++ duurzaamheid.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Duurzaamheid</title>
-    <link rel="shortcut icon" href="img/illustraties/png/Sunny_socks_green.png" />
+    <link rel="shortcut icon" href="img/illustraties/png/Sunny_socks_green.png"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style-duurzaamheid.css">
 </head>
@@ -103,8 +103,6 @@
                         <span class="process-stepnr">Stap 4</span>
                     </div>
                 </div>
-                <p>De afgewerkte sokken worden duurzaam verpakt en klaargemaakt voor distributie.</p>
-                <span class="process-label">Processtappen:</span>
                 <ul>
                     <li><i class="fa-regular fa-circle-check"></i>Eindcontrole en kwaliteitsbeoordeling</li>
                     <li><i class="fa-regular fa-circle-check"></i>Duurzame verpakking van gerecyclede materialen</li>
