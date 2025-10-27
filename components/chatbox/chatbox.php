370ee0a700326b3be603581189838cea59c6f18c
Nathan Eleveld <nathan.eleveld@student.nhlstenden.com>
2025-10-27 11:52:29 +0100

    added .json file for the search.

--- components/chatbox/chatbox.php
+++ components/chatbox/chatbox.php
@@ -1,5 +1,25 @@
 <?php
+    $searchData = 'searchData.json';
     $response_message = 'Hoi! Waarmee kan ik je helpen?';
+    $input = "";
+
+    function getValueFromJsonFile($searchData){
+        $jsonString = file_get_contents($searchData);
+        $result_searchData = json_decode($jsonString, true);
+        return $result_searchData;
+    }
+
+    function formatInput($input){
+        $lowered_input = strtolower($input);
+        return $lowered_input;
+    }
+
+    function createResponse($searchData, $input){
+        $result_searchData = getValueFromJsonFile($searchData);
+        $formatted_input = formatInput($input);
+
+
+    }
 
     function getResponse($response_message){
         echo '
