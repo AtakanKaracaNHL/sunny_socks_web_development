412cc97ea09d653c8968578e61167318fbb791b7
Nathan Eleveld <nathan.eleveld@student.nhlstenden.com>
2025-10-28 12:00:11 +0100

    Added the chat logic.

--- components/chatbox/chatbox.php
+++ components/chatbox/chatbox.php
@@ -1,8 +1,17 @@
 <?php
+    $input = filter_input(INPUT_POST, 'chat-input');
     $searchData = 'searchData.json';
-    $response_message = 'Hoi! Waarmee kan ik je helpen?';
-    $input = "";
 
+    if($input == null){
+        $input = "eerste bericht";
+    }
+?>
+
+<form action="./chatbox.php" method="post">
+    <input type="text" name="chat-input" id="chat-input">
+</form>
+
+<?php
     function getValueFromJsonFile($searchData){
         $jsonString = file_get_contents($searchData);
         $result_searchData = json_decode($jsonString, true);
@@ -11,21 +20,20 @@
 
     function formatInput($input){
         $lowered_input = strtolower($input);
-        return $lowered_input;
+        $formatted_input = $lowered_input;
+        return $formatted_input;
     }
 
     function createResponse($searchData, $input){
         $result_searchData = getValueFromJsonFile($searchData);
         $formatted_input = formatInput($input);
 
-
+        if(array_key_exists($formatted_input, $result_searchData)){
+            return $result_searchData[$formatted_input];
+        }else{
+            return "Helaas begrijp ik uw bericht niet. Ik advisseer om contact op te nemen met de klantenservice.";
+        }
     }
 
-    function getResponse($response_message){
-        echo '
-            <div class="response-item">' . $response_message . '</div>
-        ';
-    }
-
-    getResponse($response_message);
+    echo (createResponse($searchData, $input));
 ?>
