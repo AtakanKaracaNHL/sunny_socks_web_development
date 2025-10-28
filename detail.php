41b82b8bbbff6650aaf3d399bbc8513ef8c77559
Friso-klinge <friso.klinge@student.nhlstenden.com>
2025-10-28 10:18:09 +0100

    cleaned up code

--- detail.php
+++ detail.php
@@ -43,13 +43,13 @@
         </h1>
     </div>
     <div class="grid-5">
-        <img src="<?php echo $src;?>" alt="hi" class=display_img>
+        <img src="<?php echo $src;?>" alt="Sock_img" class=display_img>
         <div class="info">
             <h2>Sunny socks</h2>
             <div class="spacebetween">
                 <h1><?php echo $name;?></h1>
                 <div>
-                    <img src="img\illustraties\png\Heart.png" alt="">
+                    <img src="img\illustraties\png\Heart.png" alt="Heart">
                 </div>
             </div>
             <img src="img/sterren.png" alt="Sterren">
