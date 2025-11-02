9e95daa65cc525a89d0fc1a26736a8960f554b26
Atakan Karaca <atakan.karaca@student.nhlstenden.com>
2025-11-02 14:48:57 +0100

    Style link fix

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <title>contact</title>
     <link rel="shortcut icon" href="img/illustraties/png/Sunny_socks_green.png"/>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="/sunny_socks_web_development/css/contactpagina.css">
+    <link rel="stylesheet" href="css/contactpagina.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/header.css">
 </head>
